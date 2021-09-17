14f057d3c551204c897adf2effb99c60aff184e0
Leonardo Vargas Schilling <leonardo.schilling@cwi.com.br>
2021-09-17 00:41:34 -0300

    algoritmo de compra e venda de acoes

---
+++ src/main/java/Main.java
@@ -0,0 +1,10 @@
+import algorithm.StockTradingBrokerAlgorithm;
+
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        final var stockTradingBrokerAlgorithm = new StockTradingBrokerAlgorithm();
+        stockTradingBrokerAlgorithm.run();
+    }
+}
